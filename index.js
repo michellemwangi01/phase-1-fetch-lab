2646d90f236e1ba5b4a14814a8dc4147b37dccf1
michellemwangi01 <michellemwangim@gmail.com>
2023-06-21 23:51:23 +0300

    completed

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return (fetch('https://anapioficeandfire.com/api/books')
+  .then((res)=> {return res.json() })
+  .then ((book) => {return renderBooks(book)}))
+
   
 }
 
